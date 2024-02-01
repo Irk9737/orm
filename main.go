19f8a9a5ed8b7db283c44e15c576f046078c28d4
John Wong <151629107+irk9737@users.noreply.github.com>
2024-02-02 04:45:40 +0800

    添加 yaml

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"gopkg.in/yaml.v2"
 	"net/http"
+	"os"
+	"strings"
 	"time"
 	"xorm.io/xorm"
 )
@@ -19,9 +22,18 @@ type XormValue struct {
 	Xorm *xorm.Engine
 }
 
-type OrmValue struct {
-	xVal XormValue
+type OrmValue[T any] struct {
+	//Cfg Config
+	Key    string
+	sqlStr string
+	XVal   XormValue
 }
+
+type Repository interface {
+	InsertData(ctx *gin.Context)
+}
+
+var _ Repository = &OrmValue[any]{}
 
 // Stu 定义结构体(xorm 支持双向映射)：没有表会进行创建
 type Stu struct {
@@ -40,30 +52,95 @@ type XormResponse struct {
 	Data    any    `json:"data"`
 }
 
-func init() {
-	sqlStr := "root:pwd@tcp(127.0.0.1:3306)/xorm?charset=utf8mb4&parseTime=true&loc=Local" // xorm: 数据库名称
+func (orm *OrmValue[T]) GetDB() error {
 	var err error
 	// 1、创建数据库引擎
-	x, err = xorm.NewEngine("mysql", sqlStr)
+	x, err = xorm.NewEngine("mysql", orm.sqlStr)
 	if err != nil {
 		fmt.Println("数据库连接失败:", err)
 	}
 	// 2、创建或同步表 Stu
-	err = x.Sync(new(Stu))
+	err = x.Sync(new(T))
 	if err != nil {
 		fmt.Println("数据库同步失败:", err)
 	}
+	orm.XVal.Xorm = x
+
+	return err
+}
+
+type Config struct {
+	DB struct {
+		MySQL string `yaml:"mysql"`
+	} `yaml:"db"`
+	Business struct {
+		Key string `yaml:"key"`
+	} `yaml:"business"`
+}
+
+func (orm *OrmValue[T]) GetCfg() error {
+	// 读取YAML文件
+	yamlFile, err := os.ReadFile("config/cfg.yaml")
+	if err != nil {
+		fmt.Printf("无法读取YAML文件：%v", err)
+		return err
+	}
+
+	// 解析YAML文件
+	var cfg Config
+	err = yaml.Unmarshal(yamlFile, &cfg)
+	if err != nil {
+		fmt.Printf("无法解析YAML文件：%v", err)
+		return err
+	}
+
+	orm.Key = cfg.Business.Key
+	orm.sqlStr = cfg.DB.MySQL
+	return nil
 }
 
 func main() {
 	r := gin.Default()
+
+	db := OrmValue[Stu]{}
+	err := db.GetCfg()
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.GetDB()
+	if err != nil {
+		panic(err)
+	}
+
+	//db.Key = "stu_num"
+
 	// 数据库的 CRUD ---> gin 的 POST GET PUT DELETE 方法
-	r.POST("xorm/insert", XormInsertData[Stu])
-	r.GET("xorm/get", XormGetData[Stu])
+	r.POST("xorm/insert", db.InsertData)
+	r.GET("xorm/get", db.GetData)
 	r.GET("xorm/mulget", XormGetMulData[Stu])
 	r.PUT("xorm/update", XormUpdateData)
 	r.DELETE("xorm/delete", XormDeleteData[Stu])
 	r.Run(":8080")
+}
+
+// InsertData 插入操作
+func (orm *OrmValue[T]) InsertData(c *gin.Context) {
+	var typ T
+	err := c.Bind(&typ)
+	if err != nil {
+		HandleResponse(c, http.StatusBadRequest, "参数错误", "error")
+		return
+	}
+	// affected：受影响记录行数
+	affected, err := orm.XVal.Xorm.Insert(typ) // TODO 解耦
+	if err != nil || affected <= 0 {
+		fmt.Printf("insert failed, err:%v\n", err)
+		HandleResponse(c, http.StatusBadRequest, "写入失败", err)
+		return
+	}
+	HandleResponse(c, http.StatusOK, "写入成功", "OK")
+	fmt.Println(affected) // 打印结果
 }
 
 // XormUpdateData 删除操作
@@ -124,40 +201,32 @@ func XormGetMulData[T any](c *gin.Context) {
 	HandleResponse(c, http.StatusOK, "查询成功", typs)
 }
 
-func QueryData[T any](c *gin.Context) ([]T, error) {
-	stuNum := c.Query("stu_num")
+func (orm *OrmValue[T]) QueryData(c *gin.Context) ([]T, error) {
+	stuNum := c.Query(orm.Key)
 	var types []T
-	err := x.Where("stu_num=?", stuNum).Find(&types)
+	var sb strings.Builder
+	sb.WriteString(orm.Key + "=?")
+
+	types, err := orm.FindVal(sb.String(), stuNum)
+
 	return types, err
 }
 
-// XormGetData 查询操作(单条记录)
-func XormGetData[T any](c *gin.Context) {
-	typs, err := QueryData[T](c)
+func (orm *OrmValue[T]) FindVal(query any, args ...any) ([]T, error) {
+	var types []T
+	err := x.Where(query, args...).Find(&types)
+
+	return types, err
+}
+
+// GetData 查询操作(单条记录)
+func (orm *OrmValue[T]) GetData(c *gin.Context) {
+	typs, err := orm.QueryData(c)
 	if err != nil {
 		HandleResponse(c, http.StatusBadRequest, "查询错误", "error")
 		return
 	}
 	HandleResponse(c, http.StatusOK, "查询成功", typs)
-}
-
-// XormInsertData 插入操作
-func XormInsertData[T any](c *gin.Context) {
-	var typ T
-	err := c.Bind(&typ)
-	if err != nil {
-		HandleResponse(c, http.StatusBadRequest, "参数错误", "error")
-		return
-	}
-	// affected：受影响记录行数
-	affected, err := x.Insert(typ)
-	if err != nil || affected <= 0 {
-		fmt.Printf("insert failed, err:%v\n", err)
-		HandleResponse(c, http.StatusBadRequest, "写入失败", err)
-		return
-	}
-	HandleResponse(c, http.StatusOK, "写入成功", "OK")
-	fmt.Println(affected) // 打印结果
 }
 
 func HandleResponse(c *gin.Context, code int, message string, data interface{}) {
